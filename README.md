0f87e02f5a4f3bf4ef2f653994b87e460ec0cdcb
Sergey Kondratenko <Sergey.Kondratenko@ing.com>
2020-03-05 22:18:52 +0100

    generate docs

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 [![Built with open-wc recommendations](https://img.shields.io/badge/built%20with-open--wc-blue.svg)](https://github.com/open-wc)
 
+This small app I have made for my kids and my wife.
+It makes it possible to listen to audio fairy tales with one click.
+
 ## Scripts
 
 - `start` runs your app for development, reloading on file changes
---
+++ docs/sw.js
@@ -0,0 +1,51 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "index-9ecb5835.js",
+    "revision": "6ddd78520cc68e413fde2d6bc9ca5d41"
+  },
+  {
+    "url": "index.html",
+    "revision": "8691448786852ff5271304bc27e75970"
+  },
+  {
+    "url": "polyfills/custom-elements-es5-adapter.84b300ee818dce8b351c7cc7c100bcf7.js",
+    "revision": "cff507bc95ad1d6bf1a415cc9c8852b0"
+  },
+  {
+    "url": "polyfills/dynamic-import.b745cfc9384367cc18b42bbef2bbdcd9.js",
+    "revision": "ed55766050be285197b8f511eacedb62"
+  },
+  {
+    "url": "polyfills/webcomponents.d67d6757b8cd44bc35b021ee0b71d197.js",
+    "revision": "e90bc4e3aa3ed407808c7b2726581d33"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"));
